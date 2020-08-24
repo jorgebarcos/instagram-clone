7d113c15ccbac0076037f4cad0a7ea8dea89a454
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-08-24 20:39:25 -0300

    add header logo

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola este es el clone de instagram</h1>
-
+    <div className="app">
+      
+      {/* Header */}
       <div className="app__header">
         <img 
         className="app__headerImage"
         src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" 
         alt="Logo"/>
       </div>
-      {/* Header */}
+      
 
       {/* Posts */}
       {/* Posts */}
+
 
     </div>
   );
